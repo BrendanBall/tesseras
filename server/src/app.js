aa56b1615677ad2c24a22b3ae658715fa5b13b4d
Brendan Ball <ball.brendan50@gmail.com>
2018-10-01 21:25:06 +0200

    upgrade dependencies

--- server/src/app.js
+++ server/src/app.js
@@ -1,19 +1,18 @@
 import express from 'express'
-import { graphqlExpress } from 'apollo-server-express'
 import morgan from 'morgan'
 import Knex from 'knex'
+import { ApolloServer } from 'apollo-server-express'
 import createSchema from './graphql'
 import knexConfig from '../knexfile'
 import { createLoaders } from './graphql/resolvers'
-const expressPlayground = require('graphql-playground-middleware-express').default
 
 const knex = Knex(knexConfig.development)
 const schema = createSchema()
 
+const apollo = new ApolloServer({ schema, context: { loaders: createLoaders(knex), knex } })
 const app = express()
 app.use(morgan('combined'))
 
-app.get('/graphql/playground', expressPlayground({ endpoint: 'graphql' }), (req, res) => { })
-app.use('/graphql', express.json(), graphqlExpress(req => ({ debug: true, schema, context: { loaders: createLoaders(knex), knex } })))
+apollo.applyMiddleware({ app })
 
 app.listen(3000, () => console.log('running graphql server on port 3000'))
